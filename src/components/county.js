70ebeedd28c50557d54e2442774950c06b83afbd
Ian Cossentino <icossentino@gmail.com>
2021-05-10 15:57:58 -0400

    sorted output from controller, uncommented counties view

--- src/components/county.js
+++ src/components/county.js
@@ -8,41 +8,23 @@ import PropTypes from 'prop-types'
 const County = ({ county }) => {
   return (
     <div className="container col-span-1">
-      I am a county
-      <span>{county.id}</span>
-      {/* <div className="relative bg-white py-6 px-6 rounded-xl my-4 shadow-md">
-        <span className="text-xl font-semibold my-2">{state.attributes.name}</span>
+      <div className="relative bg-white py-6 px-6 rounded-xl my-4 shadow-md">
+        <span className="text-xl font-semibold my-2">{county.attributes.name}</span>
         <div className="mt-2">
           <p className="flex justify-between">
             <span className="text-sm font-semibold">Total Cases:</span>
-            <span className="text-sm">{state.attributes.total_cases}</span>
+            <span className="text-sm">{county.attributes.total_cases}</span>
           </p>
           <p className="flex justify-between">
             <span className="text-sm font-semibold">Population:</span>
-            <span className="text-sm">{state.attributes.population}</span>
+            <span className="text-sm">{county.attributes.population}</span>
           </p>
           <p className="flex justify-between">
             <span className="text-sm font-semibold">Cases as % of Pop:</span>
-            <span className="text-sm">{state.attributes.case_rate}</span>
+            <span className="text-sm">{county.attributes.case_rate}</span>
           </p>
         </div>
-        <div className="flex justify-between mt-4">
-          <div className="flex items-center text-sm p-2 rounded-sm mx-auto bg-green-100 shadow-xs cursor-pointer hover:bg-green-400 hover:text-gray-100">
-            <Link to={`/states/${state.id}/counties`}>
-              <button type="button" className="show-state-detail btn" value={state.id}>
-                County Breakdown
-              </button>
-            </Link>
-          </div>
-          <div className="flex items-center text-sm p-2 rounded-sm mx-auto bg-green-100 shadow-xs cursor-pointer hover:bg-green-400 hover:text-gray-100">
-            <Link to={`/states/${state.id}/counties`}>
-              <button type="button" className="show-graph btn" value={state.id}>
-                Cases over time
-              </button>
-            </Link>
-          </div>
-        </div>
-      </div> */}
+      </div>
     </div>
   )
 }
